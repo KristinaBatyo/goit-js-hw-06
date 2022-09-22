e91c27b31a3c289544310e1f8e114955d484ff3c
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-09-22 13:17:00 +0300

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,4 +8,12 @@ const ingredients = [
   'Condiments',
 ];
 
-
+const ingredientsEl = document.querySelector("#ingredients");
+const createEl = ingredients.map((el) => {
+  const items = document.createElement('li');
+  items.classList.add("item");
+  items.textContent = el;
+  return items;
+});
+ingredientsEl.append(...createEl);
+console.log(ingredientsEl)
