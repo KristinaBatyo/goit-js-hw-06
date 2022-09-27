28e82c63f9598aad451ec1e06bc2772f5e7dc2b3
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-09-28 00:05:28 +0300

    05

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,12 @@
 const input = document.querySelector("#name-input");
-const name = document.querySelector("#name-output");
+const output = document.querySelector("#name-output");
 console.dir(input);
 
 input.addEventListener('input', (event) => {
 if (event.currentTarget.value !== '') {
-    name.textContent = event.currentTarget.value;
+    output.textContent = event.currentTarget.value;
 } else {
     output.textContent = 'Anonymous';
 }
 });
+
